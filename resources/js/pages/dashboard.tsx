b3acc476ba6748e454b047d58bf670d7feea3041
Abdelilah07 <mabroukabdelilah07@gmail.com>
2025-04-24 22:16:50 +0100

    Some visual changes.

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -74,8 +74,8 @@ export default function Dashboard({ summaryData, lowStockMedicines, expiredMedic
 
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
-            <Head title="Dashboard" /> {/* Simplified Title */}
-            <div className="flex h-full flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+            <Head title="Dashboard" />
+            <div className="flex h-full flex-1 flex-col gap-4 p-4 md:p-8">
                 {/* Stats Cards */}
                 <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
                     {' '}
